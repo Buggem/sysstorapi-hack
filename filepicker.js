64d4ae2c73c7d0250103bd42b496d9c359cee223
Max Parry <66670928+Buggem@users.noreply.github.com>
2023-12-22 12:32:11 +1100

    Create filepicker.js

---
+++ filepicker.js
@@ -0,0 +1,64 @@
+window.showOpenFilePicker = function(fileoptions) {
+    console.log("here")
+    window.openfileopt = fileoptions;
+    return new Promise((resolve) => {
+        
+        alert("inside promise openfilepicker")
+        resolve([{
+            
+            getFile: function() {
+                alert("got to getfile")
+                
+                return new Promise((resolve) => {
+                    let input = document.createElement('input');
+                    input.type = 'file';
+                    input.style = "display: none";
+                    input.onchange = e => {
+                        resolve(e.target.files[0]);
+                    }
+                    input.click();
+                    window.myFileInput = input; // for debugging
+                }); // our part is done, we can let the file class do the rest... woo-hoo
+            }
+        }])
+    });
+    
+};
+window.showSaveFilePicker=function(fileoptions) {
+    window.savefileopt = fileoptions;
+    return new Promise((resolve) => {
+    resolve({
+            
+            kind: "file",
+            createWritable: function() {
+                
+                return new Promise((resolve) => {
+                    resolve({
+                        write: function(blob){
+                            var saveData = function (blob, fileName) {
+                                    var a = document.createElement("a");
+                                    document.body.appendChild(a);
+                                    a.style = "display: none";
+                                    
+                                            var url = window.URL.createObjectURL(blob);
+                                            a.href = url;
+                                            a.download = prompt("Choose a name for the download", fileName);
+                                            a.click();
+                                            window.URL.revokeObjectURL(url);
+                                    
+                            };
+                            saveData(blob, savefileopt.suggestedName)
+                            resolve();
+                        },
+                        close:function(){},
+                        getFile: function() {
+                            
+                            return new File(); // lol
+                        }
+                    })
+                    //fake the fake file handles fake writable
+                });
+            }
+        }) //fake the file handle
+  });
+};
